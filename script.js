45af1d685f20dcc39992fca5b496816dbd8ebcc4
Amanda Tree <amandasimonds9@gmail.com>
2019-10-07 21:14:26 -0600

    outline script

---
+++ script.js
@@ -0,0 +1,59 @@
+
+$(document).ready(function() {
+
+
+    //capture HTML elements
+
+    //timeblocks ----- append time blocks to make it more simple?
+
+
+
+
+    //add event listeners to elements
+
+    //when you click on a timeblock, add a "day event"
+
+    //----"add event" button----
+
+
+
+
+    //add elements to page
+
+    //append inputted text to time blocks
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
